e088b7f77d6d8d149cd529bee27e2415c58decb4
AnushkaJindal09 <ANUSHKA.23FE10CAI00092@muj.manipal.edu>
2026-04-01 22:00:19 +0530

    improve news query

--- ml_backend/app.py
+++ ml_backend/app.py
@@ -189,7 +189,7 @@ def get_news():
     try:
         company = request.args.get("company", "")
 
-        url = f"https://gnews.io/api/v4/search?q={company}&lang=en&token={os.getenv('GNEWS_API_KEY')}"
+        url = f"https://gnews.io/api/v4/search?q={company} stock india&lang=en&token={os.getenv('GNEWS_API_KEY')}"
         response = requests.get(url)
         data = response.json()
 
